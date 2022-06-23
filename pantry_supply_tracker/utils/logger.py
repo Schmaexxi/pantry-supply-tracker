c856c0ce462ba175685209c60d3993688f992cfd
Schmaexxi <langknecht@stud.uni-heidelberg.de>
2022-06-23 21:57:06 +0200

    Update setup.py; add fastapi-app

---
+++ pantry_supply_tracker/utils/logger.py
@@ -0,0 +1,48 @@
+import logging
+from pathlib import Path
+from logging.handlers import TimedRotatingFileHandler
+from typing import Optional
+
+
+def setup_logger(
+    name: str,
+    level: int = logging.INFO,
+    interval_type: str = "midnight",
+    interval: int = 1,
+    backup_count: int = 14,
+    filename: Optional[str] = None,
+) -> logging.Logger:
+    """setup a streaming and file-based rotating logger
+    Args:
+        name: name of the logger (also used as filename)
+        level: loglevel
+        interval_type: type of interval. Possible values: 'S', 'M', 'H', 'D',
+        'W0'-'W6', 'midnight'
+        interval: number of occurrences until handler uses new file
+        backup_count: number of files to keep at one time
+        filename: base name of the log file
+    Returns:
+        created logger
+    """
+
+    if filename is None:
+        (Path().cwd() / "logs").mkdir(exist_ok=True)
+        filename = f"logs/{name}.log"
+    logging.basicConfig(
+        level=level,
+        handlers=[
+            logging.StreamHandler(),
+            TimedRotatingFileHandler(
+                filename,
+                when=interval_type,
+                interval=interval,
+                backupCount=backup_count,
+            ),
+        ],
+        format=(
+            "%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s:"
+            " %(message)s"
+        ),
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    return logging.getLogger(name)
